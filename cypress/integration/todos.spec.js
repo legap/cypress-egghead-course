d0219537bb77bf1004b75b564d4f2108ba73686a
Pascal Gafner, PF <pascal.gafner@postfinance.ch>
2019-06-13 21:22:32 +0200

    usage of data-cy attribute

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -2,15 +2,22 @@ describe('Todo Application', () => {
     it('loads the page', () => {
         cy.visit('/')
 
-        cy.get('.todo-list li:nth-child(1)')
+        cy.get('[data-cy=todo-item-3]')
           .should('have.text', 'Hello world')
           .should('not.have.class', 'completed')
           .find('.toggle')
           .should('not.be.checked')
 
-        cy.get('.todo-list li:nth-child(2)')
+        cy.get('[data-cy=todo-item-4]')
           .should('have.text', 'Goodnight moon')
           .should('have.class', 'completed')
+          .find('.toggle')
+          .should('be.checked')
+
+      // fails because contains selects the label and not the checkbox and find toggle can't be found
+      cy.contains('Goodnight moon')
+          .should('have.text', 'Goodnight moon')
+          .should('not.have.class', 'completed')
           .find('.toggle')
           .should('be.checked')
     })
